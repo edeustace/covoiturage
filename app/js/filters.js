f91a8c9569eb5c361552d65e774274978560133f
alexandre <alexandre@alexandre-Compaq-615.(none)>
2013-04-18 13:17:42 +0200

    init

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,8 +2,21 @@
 
 /* Filters */
 
-angular.module('phonecatFilters', []).filter('checkmark', function() {
-  return function(input) {
-    return input ? '\u2713' : '\u2718';
+angular.module('covoiturageFilter', []).filter('auto', function() {
+  return function(subscribers, filter) {
+    	if(filter){
+		var result = new Array();				
+		markers = [];
+		for(var i=0;i<subscribers.length;i++){
+			var subscriber = subscribers[i];
+			var type = subscriber.type;
+			if(type === filter){
+				result.push(subscriber);
+			}
+		}
+		return result;
+	}else{
+		return subscribers;
+	}
   };
 });
