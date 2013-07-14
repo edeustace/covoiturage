18ce7d40f9ad94decf24f1a48ef8332eceb87708
alexandre <adelegue@hotmail.com>
2013-07-14 21:57:09 +0200

    cd

---
+++ app/controllers/decorators/EventLight.java
@@ -0,0 +1,70 @@
+package controllers.decorators;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import models.Address;
+import models.Event;
+
+public class EventLight {
+	private Event event;
+    private List<Link> links = new ArrayList<Link>();
+
+    public EventLight(Event event) {
+        if(event==null){
+            throw new IllegalArgumentException("Event is required");
+        }
+        this.event = event;
+        this.links.add(Link.link(Link.SELF, controllers.routes.EventCtrl.getEvent(this.event.getId()).toString()));
+        this.links.add(Link.link("page", controllers.routes.EventCtrl.evenement(this.event.getId()).toString()));
+        this.links.add(Link.link("subscribers", controllers.routes.SubscriberCtrl.list(this.event.getId()).toString()));
+        this.links.add(Link.link("pictoFinish", controllers.routes.Assets.at("icons/finish.png").toString()));
+        this.links.add(Link.link("pictoCarDark", controllers.routes.Assets.at("icons/car_dark.png").toString()));
+        this.links.add(Link.link("pictoCar", controllers.routes.Assets.at("icons/car_classic.png").toString()));
+        this.links.add(Link.link("pictoCarLight", controllers.routes.Assets.at("icons/car_light.png").toString()));
+        this.links.add(Link.link("pictoStopDark", controllers.routes.Assets.at("icons/pedestriancrossing_dark.png").toString()));
+        this.links.add(Link.link("pictoStop", controllers.routes.Assets.at("icons/pedestriancrossing_classic.png").toString()));
+        this.links.add(Link.link("pictoStopLight", controllers.routes.Assets.at("icons/pedestriancrossing_light.png").toString()));
+        this.links.add(Link.link("pictoDontKnow", controllers.routes.Assets.at("icons/symbol_blank_jaune_dark.png").toString()));
+        this.links.add(Link.link("pictoDontKnowLight", controllers.routes.Assets.at("icons/symbol_blank_jaune_def.png").toString()));
+    }
+
+    @JsonProperty("links")
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    @JsonProperty("id")
+    public String getId() {
+        return event.getId();
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return event.getName();
+    }
+
+    @JsonProperty("description")
+    public String getDescription() {
+        return event.getDescription();
+    }
+
+    @JsonProperty("fromDate")
+    public Date getFromDate() {
+        return event.getFromDate();
+    }
+
+    @JsonProperty("toDate")
+    public Date getToDate() {
+        return event.getToDate();
+    }
+
+    @JsonProperty("address")
+    public Address getAddress() {
+        return event.getAddress();
+    }
+
+}
